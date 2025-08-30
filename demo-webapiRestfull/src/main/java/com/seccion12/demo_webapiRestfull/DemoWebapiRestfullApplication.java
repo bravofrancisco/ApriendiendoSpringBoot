623155f23b72bd8cdc390f93e200e14da25354c4
Francisco Bravo <fcobravo.23@gmail.com>
2025-08-29 20:33:23 -0400

    crud restapi

---
+++ demo-webapiRestfull/src/main/java/com/seccion12/demo_webapiRestfull/DemoWebapiRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.seccion12.demo_webapiRestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebapiRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebapiRestfullApplication.class, args);
+	}
+
+}
---
+++ demo-webapiRestfull/src/test/java/com/seccion12/demo_webapiRestfull/DemoWebapiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seccion12.demo_webapiRestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebapiRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
